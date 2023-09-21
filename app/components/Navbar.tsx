b4174aaacdd57034173bd51c09f0907bce0bcc97
AngelLam1130 <anqi_lin1130@163.com>
2023-09-21 11:19:34 -0700

    add my project and api editing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,8 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink'
-import { Bars3Icon,XMarkIcon } from '@heroicons/react/20/solid';
-
+import { Bars3Icon,XMarkIcon } from '@heroicons/react/24/solid';
+import { MenuOverlay } from './MenuOverlay';
 
 const navLinks = [
     {
@@ -23,7 +23,7 @@ const navLinks = [
 export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
             <Link 
                 href={"/"} 
@@ -32,17 +32,21 @@ export const Navbar = () => {
                 LOGO
             </Link>
             <div className='mobile-menu block md:hidden'>
-                {
-                    navbarOpen ? (
-                        <button className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                { ! navbarOpen ? (
+                        <button 
+                            onClick={() => setNavbarOpen(true)} 
+                            className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+                        >
                             <Bars3Icon className='h-5 w-5'/>
                         </button>
-                    ):(
-                        <button className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                ):(
+                        <button 
+                            onClick={() => setNavbarOpen(false)} 
+                            className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+                        >
                             <XMarkIcon className='h-5 w-5'/>
                         </button>
-                    )
-                }
+                )}
 
             </div>
             <div className='menu hidden md:block md:w-auto' id="navbar">
@@ -57,6 +61,7 @@ export const Navbar = () => {
                 </ul>
             </div>
         </div>
+        {navbarOpen ? <MenuOverlay links={navLinks}/>: null }
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,18 @@ import Image from 'next/image'
 import { HeroSection } from './components/HeroSection'
 import { Navbar } from './components/Navbar'
 import { Container } from 'postcss'
+import AboutSection from './components/AboutSection'
+import ProjectsSection from './components/ProjectSection'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
         <Navbar/>
-        <div className="container mx-auto px-12 py-4">
+        <div className="container mt-24 mx-auto px-12 py-4">
           <HeroSection />
+          <AboutSection />
+          <ProjectsSection />
         </div>
-     
     </main>
   )
 }
