2c2f71291b16692ba96bba365bab424be639b448
AngelLam1130 <anqi_lin1130@163.com>
2023-10-12 00:32:09 -0700

    project and resume update

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -59,7 +59,7 @@ export const HeroSection = () => {
                 <div>
                     <br></br> */}
                     <a 
-                        href='https://drive.google.com/file/d/1Ztf0pAB1BrlJLvs78KfgtyYZqIVez1jk/view'
+                        href='https://drive.google.com/file/d/11h45YcwyZTCjPPJ5F6GueMdrWnKZ9YbX/view'
                         className='mr-4 px-4 py-2 w-auto sm:w-fit rounded-full bg-gradient-to-br from-yellow-200 via-yellow-500 to-orange-500 hover:bg-slate-200 text-black text-bold'>
                         Resume
                     </a>
